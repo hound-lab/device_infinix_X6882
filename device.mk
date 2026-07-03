4d94e4c2afbec126282b6bfd81d7c6c459a7fe85
gutssnv <decryptor69@gmail.com>
2026-07-04 05:24:32 +0700

    X6882: Switch to the AIDL light HAL from lineage
    Signed-off-by: gutssnv <decryptor69@gmail.com>

--- device.mk
+++ device.mk
@@ -261,7 +261,7 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.lights-service.transsion
+    android.hardware.light-service.lineage
 
 # Linker config
 PRODUCT_VENDOR_LINKER_CONFIG_FRAGMENTS += \
@@ -453,8 +453,7 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/mediatek \
     hardware/mediatek/libmtkperf_client \
     hardware/google/interfaces \
-    hardware/google/pixel \
-    hardware/transsion
+    hardware/google/pixel
 
 # Thermal
 PRODUCT_PACKAGES += \
