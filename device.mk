c0bf8b642ad8432c2fd6a0da842bbbadb910138e
LuK1337 <priv.luk@gmail.com>
2026-07-15 12:49:47 +0700

    X6882: Switch to NCM USB tethering
    Change-Id: I20b6b2b2e26a45d8b28c7483afc83ad4da4b8643
    Signed-off-by: gutssnv <decryptor69@gmail.com>

--- device.mk
+++ device.mk
@@ -301,7 +301,6 @@ PRODUCT_PACKAGES += \
     SettingsResNoir \
     SettingsProviderResNoir \
     SystemUIResNoir \
-    TetheringConfigNoir \
     OpenDeltaOverlayMT6789 \
     WifiResNoir
 
@@ -490,6 +489,9 @@ PRODUCT_PACKAGES += \
     hostapd \
     libkeystore-wifi-hidl:64 \
     libkeystore-engine-wifi-hidl:64
+
+PRODUCT_PACKAGES += \
+    NcmTetheringOverlay
 
 # Inherit the proprietary files
 $(call inherit-product, vendor/infinix/X6882/X6882-vendor.mk)
