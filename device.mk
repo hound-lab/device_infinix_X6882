5d8507686c161ab28db0175dfafc9eacbb784eea
gutssnv <decryptor69@gmail.com>
2026-07-15 15:59:13 +0700

    Revert "Switch to AOSP Health AIDL"
    This reverts commit 4db6777be51dbde43596c1c01bfa17a2f0ae50bc.

--- device.mk
+++ device.mk
@@ -199,8 +199,8 @@ PRODUCT_PACKAGES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health-service.example \
-    android.hardware.health-service.example-recovery \
+    android.hardware.health-service.mediatek \
+    android.hardware.health-service.mediatek-recovery \
     charger_res_images_vendor
 
 # HIDL
