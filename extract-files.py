47f6960bb426810a9aaf5878fe836d5242f6719d
Nicholas Lim <niclimcy@lineageos.org>
2026-09-17 01:07:54 +0700

    X6882: Rename MediaTek libmnl to libmnl_mtk
    - libmnl is MediaTek's Navigation Library
    - Add _mtk suffix to avoid colliding with AOSP libmnl (netlink)
    
    Change-Id: I53c253a6ec721d6d385b10e8c022608424b2f0e3
    Signed-off-by: gutssnv <decryptor69@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -82,7 +82,8 @@ blob_fixups: blob_fixups_user_type = {
         .add_needed('libprocessgroup_shim.so'),
     ('vendor/lib64/mt6789/lib3a.flash.so', 'vendor/lib64/mt6789/lib3a.ae.stat.so', 'vendor/lib64/mt6789/lib3a.sensors.flicker.so', 'vendor/lib64/mt6789/lib3a.sensors.color.so', 'vendor/lib64/mt6789/libaaa_ltm.so', 'vendor/lib64/libSQLiteModule_VER_ALL.so'): blob_fixup()
         .add_needed('liblog.so'),
-    'vendor/lib64/mt6789/libmnl.so': blob_fixup()
+    'vendor/lib64/libmnl_mtk.so': blob_fixup()
+	.fix_soname()
         .add_needed('libcutils.so'),
     'vendor/lib64/mt6789/libneuralnetworks_sl_driver_mtk_prebuilt.so': blob_fixup()
         .clear_symbol_version('AHardwareBuffer_allocate')
@@ -124,6 +125,8 @@ blob_fixups: blob_fixups_user_type = {
         .add_needed('libbase_shim.so'),
     'vendor/lib64/librt_extamp_intf.so': blob_fixup()
         .replace_needed('libtinyxml2.so', 'libtinyxml2-v34.so'),
+    'vendor/bin/mnld': blob_fixup()
+        .replace_needed('libmnl.so', 'libmnl_mtk.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
